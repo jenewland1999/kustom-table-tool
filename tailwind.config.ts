55d56fdc7e170af7d97cd62a4a3496625e10010c
Jordan Newland <34895363+jenewland1999@users.noreply.github.com>
2023-12-27 16:12:36 +0000

    fix: typo in tailwindcss config
    Signed-off-by: Jordan Newland <34895363+jenewland1999@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTHeme';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
